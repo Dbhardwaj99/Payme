c96e0085800e1e32cf71c27d3864498a421561d9
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2024-07-11 13:25:44 +0530

    chore: Update dependencies and add Recoil for state management

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,21 @@
-import { PrismaClient } from "@repo/db/client"
-const prisma = new PrismaClient();
+// import { PrismaClient } from "@repo/db/client"
+// const prisma = new PrismaClient();
 
-export default function Home(): JSX.Element {
-  return(
-    <div className="text-2xl">
-      Hi from user-app
-    </div>
-  )
+// export default function Home(): JSX.Element {
+//   return(
+//     <div className="text-2xl">
+//       Hi from user-app
+//     </div>
+//   )
+// }
+
+"use client";
+
+import { useBalance } from "@repo/store/balance";
+
+export default function() {
+  const balance = useBalance();
+  return <div>
+    hi there {balance}
+  </div>
 }
